ed5ae2f04375cc37220eed5872b23eb5583f8a77
Edoardo Vanin <edoardo.vanin@gmail.com>
2018-07-14 13:46:51 +0200

    Modify the autoencoder learning rate and transform it in a adaptive learning rate

--- autoencoder.py
+++ autoencoder.py
@@ -7,15 +7,16 @@ import gzip
 from keras.layers import Input,Conv2D,MaxPooling2D,UpSampling2D, Flatten, Dense, Reshape
 from keras.models import Model
 from keras.optimizers import Adam
+form keras.callbacks import ReduceLROnPlateau
 
 
-x_validateMFCC = np.load('compressed_feature/x_validateMFCC.npy')
-y_validateMFCC = np.load('compressed_feature/y_validateMFCC.npy')
-x_testMFCC = np.load('compressed_feature/x_testMFCC.npy')
-y_testMFCC = np.load('compressed_feature/y_testMFCC.npy')
+x_validateMFCC = np.load('compressed_feature/x_validate.npy')
+y_validateMFCC = np.load('compressed_feature/y_validate.npy')
+x_testMFCC = np.load('compressed_feature/x_test.npy')
+y_testMFCC = np.load('compressed_feature/y_test.npy')
 
-x_trainMFCC = np.load('compressed_feature/x_trainingMFCC.npy', allow_pickle = False)
-y_trainMFCC = np.load('compressed_feature/y_trainingMFCC.npy', allow_pickle = False)
+x_trainMFCC = np.load('compressed_feature/x_training.npy', allow_pickle = False)
+y_trainMFCC = np.load('compressed_feature/y_training.npy', allow_pickle = False)
 
 # da ricordarsi di scalare bene test e validation
 x_trainMFCC = x_trainMFCC/np.max(x_trainMFCC)
@@ -27,9 +28,9 @@ print('x_trainMFCC shape: {}'.format(x_trainMFCC.shape))
 print('x_testMFCC shape: {}'.format(x_testMFCC.shape))
 print('x_validateMFCC shape: {}'.format(x_validateMFCC.shape))
 
-feats_shape = (99, 12, 1)
+feats_shape = (99, 40, 1)
 
-x_trainMFCC = x_trainMFCC.reshape(x_trainMFCC.shape[0], *feats_shape) 
+x_trainMFCC = x_trainMFCC.reshape(x_trainMFCC.shape[0], *feats_shape)
 x_testMFCC = x_testMFCC.reshape(x_testMFCC.shape[0], *feats_shape)
 x_validateMFCC = x_validateMFCC.reshape(x_validateMFCC.shape[0], *feats_shape)
 
@@ -38,12 +39,12 @@ print('x_trainMFCC shape: {}'.format(x_trainMFCC.shape))
 print('x_testMFCC shape: {}'.format(x_testMFCC.shape))
 print('x_validateMFCC shape: {}'.format(x_validateMFCC.shape))
 
-# Autoencoder 
+# Autoencoder
 
 batch_size = 64
 epochs = 5
 inChannel = 1
-x, y = 99, 12
+x, y = 99, 40
 input_img = Input(shape = (x, y, inChannel))
 
 def encoder(input_img):
@@ -64,24 +65,38 @@ def encoder(input_img):
     decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(up2) # 99 x 40 x 1
     return flatten, decoded
 
-flatten, decoded = encoder(input_img) 
+flatten, decoded = encoder(input_img)
 
 autoencoder = Model(input_img, decoded)
-autoencoder.compile(Adam(lr=0.05), loss = 'mean_squared_error', metrics = ['accuracy'])
+autoencoder.compile(Adam(lr=0.04), loss = 'mean_squared_error', metrics = ['categorical_accuracy'])
 
 autoencoder.summary()
 
-autoencoder_train = autoencoder.fit(x_trainMFCC, x_trainMFCC, batch_size=batch_size,epochs= 1000, verbose=1,validation_data=(x_validateMFCC, x_validateMFCC))
+learning_rate_reduction = ReduceLROnPlateau(monitor='val_acc',
+                                            patience=4,
+                                            verbose=1,
+                                            factor=0.5,
+                                            min_lr=0.00001))
+
+autoencoder_train = autoencoder.fit(x_trainMFCC, x_trainMFCC, batch_size=batch_size,
+    epochs= 1000, verbose=1,validation_data=(x_validateMFCC, x_validateMFCC)
+    callbacks = [learning_rate_reduction])
 
 enc = Model(input_img, flatten)
 encoded_feature = enc.predict(x_trainMFCC)
 
 
 clf = svm.SVC(decision_function_shape='ovr')
-clf.fit(encoded_feature, y_trainMFCC) 
+clf.fit(encoded_feature, y_trainMFCC)
 
 encoded_test = enc.predict(x_testMFCC)
 
 y_pred = clf.predict(encoded_test)
 
-np.mean(np.abs(y_pred-y_testMFCC)) 
+cnt = 0.0
+for i in range(y_test.shape[0]):
+    if(y_test[i] != y_pred[i]):
+        cnt += 1
+
+perc_errors = cnt / y_test.shape[0]
+print(perc_errors)
